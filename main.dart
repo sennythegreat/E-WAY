05899d0526803327b54666c39de8a2d6c4d534f6
sennythegreat <sduro@gbox.adnu.edu.ph>
2025-03-16 23:17:48 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your App Title',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(), // Set HomePage as the landing page
+      debugShowCheckedModeBanner: false, // Disable the debug banner
+    );
+  }
+}
